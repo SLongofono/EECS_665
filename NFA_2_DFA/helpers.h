5d5343c96c3c141d411e00495502dbc5af6669a2
Stephen <onofognol@ku.edu>
2017-10-07 22:40:36 -0500

    Starting over with standard data types

--- NFA_2_DFA/helpers.h
+++ NFA_2_DFA/helpers.h
@@ -1,23 +1,48 @@
-/*
- * Helper Functions for the NFA to DFA project
- */
+#ifndef HELP_H
+#define HELP_H
 
-#ifndef HELPERS_H
-#define HELPERS_H
-#include <vector>
+#include <stdio.h>
+#include <iostream>
 #include <cassert>
-#include <string>
-#include <stdlib.h>
-#include "State.h"
+#include <vector>
+
+// Map lowercase letters to [0, 25] and 'E' to 26
+int c_map(char c){
+	int num = c;
+	std::cout << "Mapping " << num << std::endl;
+	assert( ( c < 123 && c > 96 ) || (c == 'E') );
+	if('E' == c){
+		return 26;
+	}
+	return (c-97);
+}
+
+// Undo map for printing
+char c_unmap(int i){
+	if(26 == i){
+		return 'E';	
+	}
+	return (i+97);
+}
+
+
+// Prints an int vector on a single line, no return
+void print_int_vec(std::vector<int> *v){
+	std::cout << "{ ";
+	for(std::vector<int>::iterator i = v->begin(); i != v->end(); ++i){
+		std::cout << *(i) << " ";
+	}
+	std::cout << "} " << std::endl;
+}
 
 
 // Credit to Adam Pierce, string tokenizer for C++ without resorting to Boost
 // Modified to handle tabs and spaces alike
 // Retrieved from https://stackoverflow.com/questions/53849/how-do-i-tokenize-a-string-in-c
 // Sept. 29, 2017
-std::vector<std::string> split_str(const char *str)
+std::vector<std::string> *split_str(const char *str)
 {
-    std::vector<std::string> result;
+    std::vector<std::string> *result = new std::vector<std::string>;
 
     do
     {
@@ -26,16 +51,16 @@ std::vector<std::string> split_str(const char *str)
         while(*str != ' ' && *str != '\t' && *str)
             str++;
 
-        result.push_back(std::string(begin, str));
+        result->push_back(std::string(begin, str));
     } while (0 != *str++);
 
     return result;
 }
 
 
-std::vector<int> split_int(const char *str)
+std::vector<int> *split_int(const char *str)
 {
-    std::vector<int> result;
+    std::vector<int> *result = new std::vector<int>;
 
     do
     {
@@ -44,94 +69,11 @@ std::vector<int> split_int(const char *str)
         while(*str != ' ' && *str != '\t' && *str != ',' && *str)
             str++;
 
-        result.push_back(std::stoi(std::string(begin, str)));
+        result->push_back(std::stoi(std::string(begin, str)));
     } while (0 != *str++);
 
     return result;
 }
-
-
-/*
- * @brief       Helper function to determine if an integer is a member of a
- *              vector<int>
- * @param v     A pointer to an std::vector<int> to check for membership
- * @param n     An integer to find in v
- * @return      Returns 1 if the integer is a member of the vector, or 0
- *              otherwise
- */
-int is_member(std::vector<int> * v, int n){
-        int size = v->size();
-        for(int i = 0; i<size; ++i){
-                if(n == v->at(i)){
-                        return 1;
-                }
-        }
-        return 0;
-}
-
-
-/*
- * @brief       Finds the position of a state with name name in the given
- *              vector of states.
- * @param v     A pointer to a std::vector<State> with all known states
- * @param name  An integer representing the name of a state to check for
- * @return      An integer representing the position of the state with
- *              name name in the vector, or -1 if no such state exists.
- *              Note that the latter is an error.
- */
-int get_position(std::vector<State *> *v, int name){
-        int numstates = v->size();
-        for(int i = 0; i<numstates; ++i){
-                if( v->at(i)->m_name == name){
-                        return i;
-                }
-        }
-        return -1;
-}
-
-
-/*
- * @brief       Determines if two int vectors are equivalent sets
- * @param a     A pointer to a std::vector of ints representing a set of
- *              states
- * @param b     A pointer to a std::vector of ints representing a set of
- *              states
- * @return      Returns 1 if the sets are equivalent, or 0 otherwise
- * @notes       Assumes that both vectors are in fact sets, with no
- *              duplicate elements.  That is, if there are no duplicate
- *              members, and both sets have equal length, then if every member
- *              of a is a member of b, so too is every member of b a member of
- *              a.
- */
-int set_equivalent(std::vector<int> *a, std::vector<int> *b){
-        int sza = a->size();
-        int szb = b->size();
-        int found = 0;
-        int ret = 0;
-
-        if(sza == szb){
-
-                for(int i =0; i<sza; ++i){
-
-                        for(int j=0; j<szb; ++j){
-                                if(a->at(i) == b->at(j)){
-                                        found = 1;
-                                }
-                        }
-                        if(0 == found){
-                                std::cout << "No match for member " << a->at(i) << std::endl;
-                                break;
-                        }
-                        else{
-                                found = 0;
-                        }
-                }
-
-        }
-        return ret;
-}
-
-
 
 
 #endif
--- NFA_2_DFA/n2d.cpp
+++ NFA_2_DFA/n2d.cpp
@@ -1,302 +1,248 @@
-/* @file n2d.cpp
- * @brief       Given a specific input format representing a nondeterministic
- *              finite state automata, converts it to a deterministic finite
- *              automata and prints the result.
+/*
+ * @file 	n2d.cpp
+ * @author 	Stephen Lonogofono
+ * @created 	October 2017
+ * @brief	Given an input file representing a non-deterministic
+ * 		finite-state automata, converts it to an equivalent
+ * 		deterministic FSA.
  *
- * @author      Stephen Longofono
- * @created     September 2017
- * @notes       This program is ill-equipped to handle malformed input.
- *              See the example input for the correct form.
+ * @notes	This is my first foray into c++ with standard data types.
+ * 		So far, all our programming classes have forced us to roll
+ * 		our own data structures. We wrote everything from scratch.
+ * 		Why should you care?  It's likely that I've used a few types
+ * 		in strange ways, perhaps in ways that were not intended by the
+ * 		c++ folks.  This is my advance warning/apology for that.
  *
  */
-
+#include <iterator>
 #include <iostream>
-#include <vector>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string>
-#include <cassert>
-#include <stdexcept>
-#include "State.h"
+#include <sstream>
+#include <set>
+#include <map>
+#include <vector>
 #include "helpers.h"
 
 
 /*
- * @brief       Retrieve a vector of ints representing the States reachable
- *              on the given input symbol for the given State
- * @param s     A pointer to the State struct representing the State at hand
- * @param c     A char reprenting an input symbol from the alphabet
- * @return      A vector of ints representing the states reachable from s on c
+ * @brief 		Given the input  state, assembles and returns a set
+ * 			containing the states reachable on epsilon input
+ * @param s		An integer representing the state
+ * @param sigmaSize	The length of the alphabet
+ * @param NFA		A 2D array of std::set<int> representing the NFA table
+ * @return		A pointer to a std::set<int> containing all the states
+ * 			reachable from the input state on epsilon input
+ * @notes		Will always be non-empty, since every state's epsilon-
+ * 			closure includes itself.
  */
-std::vector<int>* get_moves(State *s, char c){
-        //TODO Cleanup memory
-        std::vector<int> * v = new std::vector<int>;
-        // For each member in the state's moves, if the input symbol matches,
-        // and it is not already in v, add its state value to v as an integer
-        int size = s->m_moves.size();
-        char* temp;
-        for(int i = 0; i< size; ++i){
-                temp = &(s->m_moves.at(i)->symbol);
-                if (c == *temp){
-                        std::cout << "Matched the input symbol..." << std::endl;
-                        if( !(is_member(v, s->m_moves.at(i)->state)) ){
-                                std::cout << "New state encountered, adding to result..." << std::endl;
-                                v->push_back(s->m_moves.at(i)->state);
-                        }
-                }
-        }
-        return v;
+std::set<int>* epsilon_closure(int s, int sigmaSize, std::set<int> ***NFA){
+	std::set<int> *ret = new std::set<int>;
+	
+	ret->insert(s);
+
+	// if there are states to be found
+	if(NFA[s][sigmaSize-1] != nullptr){
+		ret->insert( NFA[s][sigmaSize-1]->begin(), NFA[s][sigmaSize-1]->end() );
+	}	
+		
+	return ret;
 }
 
 
 /*
- * @brief       Retrieve a vector of ints representing the States reachable
- *              from a list of states on NULL input (or epsilon, or lambda,
- *              or whatever else you want to call it)
- * @param s     A pointer to all the possible states at hand
- * @param moves A pointer to a vector of ints representing states to check
- * @return      A pointer to an std::vector<int> containing all the states
- *              reachable via NULL input from the given list of states in
- *              moves.  Note that at minimum, this will be the vector moves.
+ * @brief		Given the input state and a symbol, returns a set
+ * 			containing all the states reachable on the symbol input
+ * @param s		An integer representing the state
+ * @param c		An integer representing the input symbol
+ * @param numStates	The number of states in the NFA table
+ * @param sigmaSize	The length of the alphabet
+ * @param NFA		A 2D array of std::set<int> representing the NFA table
+ * @return		A pointer to a std::set<int> representing all the states
+ *  			reachable from the input state on the input symbol.
  */
-std::vector<int>* get_epsilon_closure(std::vector<State *> *s, std::vector<int>* moves){
-        //TODO Cleanup memory
-        std::vector<int>* v = new std::vector<int>;
-        int pos, size_moves, size_transitions;
-        State* curr;
-
-        /* For each of the states given in moves:
-        *       Find the state in the list of states s
-        *               For each move in the current state
-        *                       If the move has epsilon, add the resultant
-        *                       state to the output vector.  When adding, use
-        *                       set rules.
-        */
-
-        size_moves = moves->size();
-
-        for(int i = 0; i<size_moves; ++i){
-                // Every state can reach itself on NULL input
-                if( !(is_member(v, moves->at(i)))){
-                        v->push_back(moves->at(i));
-                }
-
-                // Locate the move's state in our list of known states
-                pos = get_position(s, moves->at(i));
-                curr = s->at(pos);
-                assert(s != nullptr);
-
-                // For each symbol transition in the current state, if the
-                // symbol is 'E', add the resultant state to the results.
-                // Only do so if the state is not already in the results.
-                size_transitions = curr->m_moves.size();
-                for(int j = 0; j<size_transitions; ++j){
-                        if('E' == curr->m_moves.at(j)->symbol){
-                                if( !(is_member(v, curr->m_moves.at(j)->state))){
-                                        v->push_back(curr->m_moves.at(j)->state);
-                                }
-                        }
-                }
-
-        }
-
-        return v;
+std::set<int>* moves(int s, int c, int numStates, int sigmaSize, std::set<int> ***NFA){
+	
+	std::set<int> *ret = nullptr;
+	
+	// if there are states to be found
+	if(NFA[s][c] != nullptr){
+		ret = new std::set<int>;
+		ret->insert( NFA[s][c]->begin(), NFA[s][c]->end() );
+	}
+	return ret;
 }
 
 
-int main(int argc, char** argv){
+int main(int argc, char **argv){
+	
+	/**********************************************************************
+	 * Parsing Metadata
+	 *********************************************************************/
+	std::vector<std::string> *finalStates;	// Any of these states is accepted
+	int numStates;				// How many total states
+	int initialState;			// Where to begin
+	int sigmaSize = 27;	 		// To make things simple,
+						// assume lower case alphabet
+						// plus E
+	std::string buff;
+
+	// Fetch initial state
+	// Handle input "Initial State: {#}"
+	std::getline(std::cin, buff);
+	buff.pop_back();
+	initialState = std::stoi(buff.substr(16));
+	std::cout << "Initial state: " << initialState << std::endl;
+	
+
+	// Fetch final states as a vector
+	// Handle input "Final States: {#,#,#,#...}"
+	std::getline(std::cin, buff);
+	buff.pop_back();
+	finalStates = split_str(buff.substr(15).data());
+	int len = finalStates->size();
+	std::cout << "Final states (" << len << "): ";
+	for(int i = 0; i<len; ++i){
+		std::cout << finalStates->at(i) << " ";
+	}
+	std::cout << std::endl;
+	
+
+	// Fetch total number of states
+	// Handle input "Total States: #"
+	std::getline(std::cin, buff);
+	numStates = std::stoi(buff.substr(14));
+	std::cout << "Number of states: " << numStates << std::endl;
+	
+
+	// Fetch input alphabet and do nothing with it
+	// Handle input "State	a	b	..	E"
+	std::getline(std::cin, buff);
+	
+	std::cout << "Maximum alphabet : " << std::endl;
+	for(int i = 0; i<sigmaSize; ++i){
+		std::cout << i << " maps to " << c_unmap(i) << std::endl;
+	}
 
 
-        State testState(99);
-        testState.print_moves();
-        testState.add_move('a', 100);
-        testState.print_moves();
+	/**********************************************************************
+	 * Assemble Input States
+	 *********************************************************************/
+	
 
-        /**********************************************************************
-         * Parsing Input
-         *********************************************************************/
+	std::vector<int>* NFA_table[numStates][sigmaSize];
 
-        int numStates;                          // How many total states
-        int initialState;                       // Where to begin
-        std::vector<std::string> finalStates;   // Any of these states is accepted
-        std::vector<std::string> sigma;         // Input alphabet
+	// start blank
+	for(int i = 0; i < numStates; ++i){
+		for(int j=0; j<sigmaSize; ++j){
+			NFA_table[i][j] = nullptr;	
+		}	
+	}
 
-        std::string buff;
+	// Handle input "#	{#,#..}	{#,#...} ..."
+	std::getline(std::cin, buff);
+	while(buff.size() > 1){
+		std::cout << "Read in state: " << buff;
 
-        // Fetch initial state
-        std::getline(std::cin, buff);
-        buff.pop_back();
-        initialState = std::stoi(buff.substr(16));
-        std::cout << "Initial state: " << initialState << std::endl;
+		// Tokenize
+		std::vector<std::string> *pieces= split_str(buff.data());
+	
+		// First element is this state's name
+		int name = std::stol(pieces->at(0));
 
+		std::cout << std::endl << "Creating state: " << name <<std::endl;
 
-        // Fetch final states as a vector
-        std::getline(std::cin, buff);
-        buff.pop_back();
-        finalStates = split_str(buff.substr(15).data());
-        int len = finalStates.size();
-        std::cout << "Final states (" << len << "): ";
-        for(int i = 0; i<len; ++i){
-                std::cout << finalStates.at(i) << " ";
-        }
-        std::cout << std::endl;
+		len = pieces->size();
 
+		// For each set
+		for(int i = 1; i<len; ++i){
+			
+			// Skip empty sets
+			if(2 < pieces->at(i).size()){
+				
+				// Strip braces from ends
+				std::string temp = pieces->at(i).substr(1, pieces->at(i).size()-2);
 
-        // Fetch total number of states
-        std::getline(std::cin, buff);
-        numStates = std::stoi(buff.substr(14));
-        std::cout << "Number of states: " << numStates << std::endl;
+				std::vector<int> *transitions = split_int(temp.data());
+				
+				std::cout << "Parsed set: ";
+				print_int_vec(transitions);
+				std::cout << std::endl;
 
+				// If there is less than a full
+				// alphabet, the last will not
+				int cval;
+				if(len == 26){
+					cval = i-1;
+				}
+				else{
+					// If we don't have a full
+					// alphabet, make sure the
+					// last character is set to E
+					cval = (i == len-1) ? 26 : i-1;
+				}
 
-        // Fetch input alphabet
-        std::getline(std::cin, buff);
-        sigma = split_str(buff.substr(6).data());
-        len = sigma.size();
-        std::cout << "Sigma:  { ";
-        for(int i = 0; i< len; ++i){
-                std::cout << sigma.at(i) << " ";
-        }
-        std::cout << "}" << std::endl;
+				NFA_table[name][cval] = transitions;
 
+			}
+			else{
+				std::cout << "Empty set, moving on..." << std::endl;	
+			}
+		}
 
-        /**********************************************************************
-         * Assemble Input States
-         *********************************************************************/
+		delete pieces;
+		pieces = nullptr;
+		std::getline(std::cin, buff);
+		
+	}// End while(buff.size() > 1)
 
-        // Input states vector
-        std::vector<State *> *states = new std::vector<State *>;
+	std::cout << "Input table complete" << std::endl;
 
-        try{
-                std::cout << "Looking for more input..." << std::endl;
-                std::getline(std::cin, buff);
-                while(buff.size() > 1){
-                        std::cout << "Read in state: " << buff << std::endl;
-
-                        // Tokenize
-                        std::vector<std::string> pieces= split_str(buff.data());
-
-                        len = pieces.size();
-                        for(int i = 0; i<len; ++i){
-                                std::cout << pieces.at(i) << " ";
-                        }
-
-                        // First element is this state's name
-                        State *newState = new State(std::stol(pieces.at(0)));
-
-                        std::cout << std::endl << "Creating state " << newState->m_name <<std::endl;
-
-                        assert(sigma.size() == pieces.size() - 1);
-
-                        // For each set
-                        for(int i = 1; i<len; ++i){
-
-                                // Skip empty sets
-                                if(2 < pieces.at(i).size()){
-
-                                        // Strip braces from ends
-                                        std::string temp = pieces.at(i).substr(1, pieces.at(i).size()-2);
-
-                                        std::cout << "Read set: " << temp << std::endl;
-                                        std::vector<int> transitions = split_int(temp.data());
-                                        int length = transitions.size();
-
-                                        for(int j = 0; j<length; ++j){
-                                                std::cout << transitions.at(j) << std::endl;
-
-                                                char c[2];
-                                                int temp = sigma.at(i-1).copy(c, 1);
-                                                c[temp] = '\0';
-                                                newState->add_move(c[0], transitions.at(j));
-                                        }
-                                }
-                                else{
-                                        std::cout << "Empty set, moving on..." << std::endl;
-                                }
-                        }
-
-                        std::cout << "Completed a state.  Details: " << std::endl;
-                        newState->print_moves();
-                        std::cout << buff << std::endl;
-                        std::getline(std::cin, buff);
-                        states->push_back(newState);
-                }
-        }
-        catch(std::exception& e){
-                std::cout << e.what() << std::endl;
-        }
-
-        std::cout << "Finished parsing input.  The input states:" << std::endl;
-
-        int temp = states->size();
-        State* s;
-        for(int i = 0; i<temp; ++i){
-                s = states->at(i);
-                std::cout << "State " << s->m_name << ":" << std::endl;
-                s->print_moves();
-        }
-
-        s = states->at(1);
-        std::cout << "Getting moves for 2nd state on input a..." << std::endl;
-        std::vector<int> *damoves = get_moves(s, 'a');
-        temp = damoves->size();
-        for(int i = 0; i<temp; ++i){
-                std::cout << damoves->at(i) << std::endl;
-        }
-
-        std::cout << "Getting E-closure for 2nd state..." << std::endl;
-        std::vector<int> *daclosure = get_epsilon_closure(states, damoves);
-        temp = daclosure->size();
-        for(int i =0; i<temp; ++i){
-                std::cout << daclosure->at(i) << std::endl;
-        }
+	for(int i = 0; i< numStates; ++i){
+		for(int j = 0; j<sigmaSize; ++j){
+			
+			if(nullptr != NFA_table[i][j]){
+				std::cout << "State " << i+1 << ":" << std::endl;
+				std::cout << "\t" << "On input " << c_unmap(j) << ": ";
+				print_int_vec(NFA_table[i][j]);
+				std::cout << std::endl;
+			}
+		}
+	}
 
 
-        /**********************************************************************
-         * The Algorithm
-         *********************************************************************/
+	/**********************************************************************
+	 * Main Algorithm
+	 *********************************************************************/
+	
+	// The DFA table maps some input set to an array of output sets,
+	// indexed by the input alphabet.
+	std::map< std::set<int>, std::vector< std::set<int> > >DFA_table;
 
-        /*
-         * The start state is added to set of DFA states, unmarked, with no
-         * transitions
-         *
-         * For each unmarked state in the set of dfa states...
-         *      For each input symbol on a state
-         *              Assemble the moves on that input.
-         *              Determine the epsilon closure of the resultant list
-         *              If this list doesn't match a state in the DFA states,
-         *                      add it to the DFA state
-         *
-         * Repeat until DFA states are marked.
-         *
-         * Now, look at each of the DFA states, and if it contains one of the
-         * original final states, then it is final.
-         *
-         * Print the DFA states.
-         */
-
-        std::vector<State *> * DFA_states = new std::vector<State *>;
-        State *start = new State(initialState);
-        start->m_marked = 1;
-        DFA_states->push_back(start);
+	// Compute epsilon closure of the initial state.  This is the first
+	// DFA state.
+	
+	
+	
 
 
-
-        /**********************************************************************
-         * Housekeeping
-         *********************************************************************/
-
-
-        std::cout << "Cleaning up..." << std::endl;
-
-        temp = states->size();
-
-        for(int i = 0; i<temp; ++i){
-                s = states->at(i);
-                delete s;
-        }
+	/**********************************************************************
+	 * Housekeeping
+	 *********************************************************************/
+	
+	// Clean up finalstates
+	delete finalStates;
 
 
-        delete states;
+	// Clean up NFA_table
+	for(int i = 0; i<numStates; ++i){
+		for(int j=0; j<sigmaSize; ++j){
+			delete NFA_table[i][j];
+		}	
+	}
 
-        return 0;
+	// Clean up DFA_table
+	// TODO
 
+	return 0;
 }
